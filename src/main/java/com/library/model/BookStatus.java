f1054abdaec56b14ccacc08f2a21703522fd7158
MustafaElSayed <mustafa01148990430@gmail.com>
2026-04-11 18:21:56 +0200

    feat: implement BookStatus enum

--- src/main/java/com/library/model/BookStatus.java
+++ src/main/java/com/library/model/BookStatus.java
@@ -3,9 +3,9 @@ package com.library.model;
 /**
  * Enumerates the availability states a book can have in the catalog.
  *
- * <p>The values in this enum should support the borrow and return workflows exposed
- * by the console application.</p>
+ * <p>The values in this enum should support the borrow and return workflows.</p>
  */
 public enum BookStatus {
-    // TODO: Define availability statuses (AVAILABLE, BORROWED)
+    AVAILABLE,
+    BORROWED;
 }
